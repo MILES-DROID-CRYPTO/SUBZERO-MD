4f0c1c00bacc97e7880007abc8098df42153ae78
MILES-DROID-CRYPTO <wanzalaferdinand41@gmail.com>
2025-11-09 00:05:37 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
-  SESSION_ID: "", // "Ice~KQQg91"
+  SESSION_ID: "", // "Ice~hOXly0"
 module.exports = {
-  SESSION_ID: "Ice~KQQg91", // Put Session Id Here Inside "*"
+  SESSION_ID: "Ice~hOXly0", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "254787065390", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
